a82345f0e0f2054630bc169c4e2b47f665480f46
Érica Sousa <42494479+EriccaSousa@users.noreply.github.com>
2021-07-05 20:35:43 -0300

    delete

--- Atividade03_BCrypt/app/routes/comentarios.js
+++
@@ -1,9 +0,0 @@
-let controller = require("../controllers/comentarios");
-let controllerAuth = require("../controllers/auth.js");
-
-module.exports = function(app) {
-    app.post("/api/usuarios/", controllerAuth.checar);
-    app.post("/api/comentarios", controller.cadastrarComentarios);
-    app.get("/api/comentarios", controller.listarComentarios);
-    app.delete("/api/comentarios/:id", controller.deleteComentariosById);
-}
